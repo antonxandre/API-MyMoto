ddbb1380f30a7f714bd15d10073af0862c031f0a
Antonio Alexandre <a.a.a.j@hotmail.com.br>
2020-03-30 13:25:47 -0300

    att!

--- src/main/java/br/edu/ifal/model/Lembrete.java
+++ src/main/java/br/edu/ifal/model/Lembrete.java
@@ -6,42 +6,62 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="lembrete")
 public class Lembrete {
-	
+
 	@Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
-	
+
 	private String nome;
-	//private String descricao;
-	//private Date date;
-	
+	private String email;
+
+	public Lembrete(long id, String nome, String email) {
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+
+	}
+	// private String descricao;
+	// private Date date;
+
 	public long getId() {
 		return id;
 	}
+
 	public void setId(long id) {
 		this.id = id;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	// public String getDescricao() {
-	// 	return descricao;
+	// return descricao;
 	// }
 	// public void setDescricao(String descricao) {
-	// 	this.descricao = descricao;
+	// this.descricao = descricao;
 	// }
 	// public Date getDate() {
-	// 	return date;
+	// return date;
 	// }
 	// public void setDate(Date date) {
-	// 	this.date = date;
+	// this.date = date;
 	// }
-	
-	
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
 }
