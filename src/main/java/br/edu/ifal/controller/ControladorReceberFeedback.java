eeba26cf594c1eff0b5b8e24b9c7c813765f0061
Ollin Magno <41454237+ollinmagno@users.noreply.github.com>
2020-05-20 16:37:17 -0300

    controller

--- src/main/java/br/edu/ifal/controller/ControladorReceberFeedback.java
+++ src/main/java/br/edu/ifal/controller/ControladorReceberFeedback.java
@@ -3,6 +3,7 @@ package br.edu.ifal.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,15 +21,9 @@ public class ControladorReceberFeedback {
 	public void receberFeedback() {
 		repoFeedback.findAll();
 	}
-	
-	@RequestMapping(value = "/feedback", method = RequestMethod.POST)
+	@RequestMapping(value = "/feedback", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
 	@PostMapping("/feedback")
-	public void enviarFeedback() {
-		ReceberFeedback feedback = new ReceberFeedback();
-		 
-//		feedback.setId(id);
-//		feedback.setMensagemDoUsuario(mensagemDoUsuario);
-//		feedback.setDataDeEnvio(dataDeEnvio);
+	public void enviarFeedback(@RequestBody ReceberFeedback feedback) {
 		repoFeedback.save(feedback);
 	}
 }
