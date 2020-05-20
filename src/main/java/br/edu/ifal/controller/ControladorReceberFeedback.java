d146252b44d1066f5c95076eed1072d81885779c
Ollin Magno <41454237+ollinmagno@users.noreply.github.com>
2020-05-20 16:09:43 -0300

    annotation restcontroller

--- src/main/java/br/edu/ifal/controller/ControladorReceberFeedback.java
+++ src/main/java/br/edu/ifal/controller/ControladorReceberFeedback.java
@@ -2,15 +2,15 @@ package br.edu.ifal.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import br.edu.ifal.model.ReceberFeedback;
 import br.edu.ifal.model.ReceberFeedbackRepositorio;
 
+@RestController
 public class ControladorReceberFeedback {
 	
 	@Autowired
