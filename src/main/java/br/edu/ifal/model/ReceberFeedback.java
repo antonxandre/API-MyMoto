6b1fc9f0ea4bc13469871cc9518ee0e9fc2e9d03
Ollin Magno <41454237+ollinmagno@users.noreply.github.com>
2020-05-22 19:34:43 -0300

    add long id

--- src/main/java/br/edu/ifal/model/ReceberFeedback.java
+++ src/main/java/br/edu/ifal/model/ReceberFeedback.java
@@ -2,14 +2,20 @@ package br.edu.ifal.model;
 
 import java.time.LocalDateTime;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.CreationTimestamp;
 
 @Entity
 @Table(name = "feedback")
-public class ReceberFeedback extends Usuario {
+public class ReceberFeedback {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
 	private String mensagemDoUsuario;
 	@CreationTimestamp
 	private LocalDateTime dataDeEnvio;
@@ -25,6 +31,12 @@ public class ReceberFeedback extends Usuario {
 	}
 	public void setDataDeEnvio(LocalDateTime dataDeEnvio) {
 		this.dataDeEnvio = dataDeEnvio;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
 	}
 	
 	
