f703abf8cd0c69a5a3f216827e9a62d32a866fa2
Ollin Magno <41454237+ollinmagno@users.noreply.github.com>
2020-05-28 20:19:56 -0300

    get e post

--- src/main/java/br/edu/ifal/controller/ControladorReceberFeedback.java
+++ src/main/java/br/edu/ifal/controller/ControladorReceberFeedback.java
@@ -1,8 +1,12 @@
 package br.edu.ifal.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,13 +21,22 @@ public class ControladorReceberFeedback {
 	@Autowired
 	ReceberFeedbackRepositorio repoFeedback;
 	
+	@RequestMapping(value = "/feedback", method = RequestMethod.GET, produces = "application/json")
 	@GetMapping
-	public void receberFeedback() {
-		repoFeedback.findAll();
+	public Iterable<ReceberFeedback> findAll() {
+		return repoFeedback.findAll();
 	}
-	@RequestMapping(value = "/feedback", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
-	@PostMapping("/feedback")
-	public void enviarFeedback(@RequestBody ReceberFeedback feedback) {
-		repoFeedback.save(feedback);
+	
+	@GetMapping("/feedback/{id}")
+	public Optional<ReceberFeedback> buscarUsuarioId(@PathVariable(value = "id") long id) {
+		return repoFeedback.findById(id);
 	}
+	
+    @RequestMapping(value = "/feedback", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
+    	public ResponseEntity<ReceberFeedback> enviarFeedback(@RequestBody ReceberFeedback feedback) {
+    	repoFeedback.save(feedback);
+    	feedback.getMensagemDoUsuario();
+		return new ResponseEntity<ReceberFeedback>(feedback, HttpStatus.CREATED);
+    }
+    
 }
